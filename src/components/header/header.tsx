4390aeb12394394f77ec86d3fb10c4b852c77dda
cedrycm <cedryc.midy@gmail.com>
2021-09-21 11:43:54 -0700

    created next.js app

---
+++ src/components/header/header.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+interface headerProps {}
+
+export const header: React.FC<headerProps> = ({}) => {
+  return <div></div>;
+};
---
+++ src/components/icons/arrowDropdown.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { IconBaseProps } from "react-icons/lib";
+
+interface arrowDropdownProps extends IconBaseProps {
+  isOpen: boolean;
+}
+
+export const arrowDropdown: React.FC<arrowDropdownProps>  = ({}) => {
+  return (
+    
+    );
+}
